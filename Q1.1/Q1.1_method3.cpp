b2d6ad705b6042c89a000c449734290fb4315938
saneenKP <saneenkp2000@gmail.com>
2022-02-02 22:59:12 +0530

    Q1 method 3 added

---
+++ Q1.1/Q1.1_method3.cpp
@@ -0,0 +1,36 @@
+//No additional Data Structures used.
+//Time Complexity -  (n log(n)) - n = length of the string.
+//Space Complexity - depends on the compiler implementation of the sorting algorithm.
+
+
+
+#include<iostream>
+#include<algorithm>
+
+bool check_unique(std::string value){
+
+
+
+    //Simple linear searching mechanism by comparing each character with all other characters 
+
+    std::sort (value.begin() , value.end());
+
+    for (int i = 0; i < value.length()-1; ++i)
+    {
+        if (value[i] == value[i+1])
+            return false;
+        
+    }
+    return true;
+}
+
+int main(){
+
+    std::string value;
+    std::cout<<"Enter the string : ";
+    std::cin>>value;
+
+    std::cout<< (check_unique(value) ? "unique" : "not unique");
+}
+
+//Note : This is not an efficient algorithms but satisfies the condition of not using an additional data structure
