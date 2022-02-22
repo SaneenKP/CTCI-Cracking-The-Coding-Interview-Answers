d0948e6659a69302da839f193088384d3cc1dd1b
saneenKP <saneenkp2000@gmail.com>
2022-02-22 17:41:37 +0530

    Q1.8 method 1 completed and hence first chapter completed

--- Q1.9/method1.cpp
+++ Q1.9/method1.cpp
@@ -10,17 +10,17 @@
 bool isSubString(std::string s1 , std::string s2){
 
     int subStringCounter = 0; 
-
     //iterate over each character of the first string.
     for (int i = 0 ; i < s1.length(); i++) 
     {
         if (s1[i] == s2[subStringCounter]){ //check if the character matches the character of 2nd string.
             subStringCounter++;             //Increase the counter pointer to the next character. 
         }
-        else
+        else{
             subStringCounter = 0;           //if found a different character , get the pointer back to the first character.
             if (s1[i] == s2[subStringCounter])             // check if the different character is same as first character in the second string.
                 subStringCounter++;         // if yes increase the pointer to next character.
+        }
 
         if (subStringCounter == s2.length()) return true; // Found a substring if all the characters from second string is encountered.
     }
@@ -36,7 +36,14 @@ int main(){
     std::cout<<"Enter the string 2 : ";
     std::cin>>s2;
 
-    std::cout<<((isSubString(s1 , s2)) ? "True" : "False");
 
+    //x = wat
+    //y = erbottle
 
+    //s1 = xy
+    //s2 = yx (rotation)
+
+    //hence to check if s2 is a rotation of s1.
+    //s2 will always be the substring of s1+s1 if its a rotation.
+    std::cout<<((isSubString(s1+s1 , s2)) || (s1.length() != s2.length()) ? "True" : "False");
 }
